28feb2a6710009f27f5351459e308c83200861c0
nanalucky <nana_lucky@msn.com>
2017-05-08 01:17:45 +0800

    md5 & uri & json

--- zqdb/Program.cs
+++ zqdb/Program.cs
@@ -5,18 +5,141 @@ using System.Text;
 using System.Threading.Tasks;
 using DxComOperate;
 using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Security.Cryptography;
 
 namespace zqdb
 {
+    public class HttpParam
+    {
+        public const string KEY = @"key";
+        public const string DEVICETOKEN = @"deviceToken";
+        public const string CLIENTTYPE = @"clientType";
+        public const string USERID = @"userId";
+        public const string CLIENTTOKEN = @"clientToken";
+        public const string TIMESTAMP = @"timestamp";
+        public const string USERTOKEN = @"userToken";
+        public const string APIVER = @"apiVer";
+        public const string CLIENTVER = @"clientVer";
+        public const string SIGN = @"sign";
+        public const string BODY = @"body";
+        public const string PHONE = @"phone";
+        public const string PASSWORD = @"password";
+        
+        string strKey = @"40389d43-5823-4e4d-be5f-d6e94e45d73f";
+        string strAnd = @"&";
+        string strEqual = @"=";
+
+        public string strDeviceToken = @"";
+        public string strClientType = @"";
+        public string strUserId = @"";
+        public string strClientToken = @"";
+        public string strTimeStamp = @"";
+        public string strUserToken = @"";
+        public string strApiVer = @"";
+        public string strClientVer = @"";
+        public string strSign = @"";
+
+        public JObject joBody;
+
+        public HttpParam(string strParam)
+        {
+            int startIndex = strParam.IndexOf("=");
+            string paramUri = strParam.Substring(startIndex + 1);
+            string param = Uri.UnescapeDataString(paramUri);
+            JObject paramJo = (JObject)JsonConvert.DeserializeObject(param);
+            strDeviceToken = (string)paramJo[DEVICETOKEN];
+            strClientType = (string)paramJo[CLIENTTYPE];
+            strUserId = (string)paramJo[USERID];
+            strClientToken = (string)paramJo[CLIENTTOKEN];
+            strUserToken = (string)paramJo[USERTOKEN];
+            strApiVer = (string)paramJo[APIVER];
+            strClientVer = (string)paramJo[CLIENTVER];
+            joBody = (JObject)paramJo[BODY];
+
+            Console.WriteLine("devicetoken:{0}", strDeviceToken);
+            Console.WriteLine("clienttype:{0}", strClientType);
+            //Console.WriteLine("bodystring:{0}", (string)paramJo[BODY]);
+            Console.WriteLine("body:{0}", JsonConvert.SerializeObject(joBody));
+        }
+
+        public string GetParam()
+        {
+            strTimeStamp = Timestamp();
+            //strTimeStamp = @"1492703932428";
+            string md5 = GetMd5();
+            Console.WriteLine(md5);
+            return md5;
+        }
+
+        public static string Timestamp()  
+        {  
+            TimeSpan span = (DateTime.Now - new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime());  
+            return ((ulong)span.TotalMilliseconds).ToString();  
+        }
+
+        private static string UserMd5(string str)
+        {
+            string cl = str;
+            string pwd = "";
+            MD5 md5 = MD5.Create();//实例化一个md5对像
+            // 加密后是一个字节类型的数组，这里要注意编码UTF8/Unicode等的选择　
+            byte[] s = md5.ComputeHash(Encoding.UTF8.GetBytes(cl));
+            // 通过使用循环，将字节类型的数组转换为字符串，此字符串是常规字符格式化所得
+            for (int i = 0; i < s.Length; i++)
+            {
+                // 将得到的字符串使用十六进制类型格式。格式后的字符是小写的字母，如果使用大写（X）则格式后的字符是大写字符
+                pwd = pwd + s[i].ToString("X");
+
+            }
+            return pwd;
+        }
+
+        private string GetMd5()
+        {
+            string urlParam = APIVER + strEqual + strApiVer
+                + strAnd + BODY + strEqual + JsonConvert.SerializeObject(joBody)
+                + strAnd + CLIENTTOKEN + strEqual + strClientToken
+                + strAnd + CLIENTTYPE + strEqual + strClientType
+                + strAnd + CLIENTVER + strEqual + strClientVer
+                + strAnd + DEVICETOKEN + strEqual + strDeviceToken
+                + strAnd + TIMESTAMP + strEqual + strTimeStamp;
+
+            if (strUserId.Length > 0)
+                urlParam += strAnd + USERID + strEqual + strUserId;
+
+            if (strUserToken.Length > 0)
+                urlParam += strAnd + USERTOKEN + strEqual + strUserToken;
+
+            urlParam += strAnd + KEY + strEqual + strKey;
+            Console.WriteLine("param:{0}", urlParam);
+
+            return UserMd5(urlParam);
+        }
+    };
+
+    public class Player
+    {
+        public void Run(string strLoginParam)
+        {
+            HttpParam param = new HttpParam(strLoginParam);
+            param.GetParam();
+        }
+    };
+    
     class Program
     {
         static void Main(string[] args)
         {
             string strMyOrder = File.ReadAllText(System.IO.Directory.GetCurrentDirectory() + "\\myorder.txt");
+            Player player = new Player();
+            player.Run(strMyOrder);
+           
 
-            Console.Write(strMyOrder);
+            Console.WriteLine("timestamp:{0}", HttpParam.Timestamp());
+
             Console.Read();
-
         }
     }
 }
